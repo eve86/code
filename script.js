69731a7a5cf54b7b13feec8bc654eca8fc5f60c6
eve <46179055+eve86@users.noreply.github.com>
2020-05-03 20:25:27 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,35 @@
-
-  $(".btn").click(function () {
-    $(".fade").fadeIn();
-    $(".modal").fadeIn();
-  })
+$(".btn").click(function () {
+  $(".fade").fadeIn();
+  $(".modal").fadeIn();
+})
   $(".time").click(function () {
-    $(".modal").fadeOut();
-    $(".fade").fadeOut();
-  })
-  $(".q-item-container").click(function () {
-    var $answer = $(this).find('.q-hide');
-    if ($answer.hasClass('open')) {
-      $answer.removeClass('open');
-      $answer.slideUp();
-      $(this).find('span').text('+');
-    } else {
-      $answer.addClass('open');
-      $answer.slideDown();
-      $(this).find('span').text('-');
-    }
+  $(".modal").fadeOut();
+  $(".fade").fadeOut();
+})
+$(".q-item-container").click(function () {
+  var $answer = $(this).find('.q-hide');
+  if ($answer.hasClass('open')) {
+    $answer.removeClass('open');
+    $answer.slideUp();
+    $(this).find('span').text('+');
+  } else {
+    $answer.addClass('open');
+    $answer.slideDown();
+    $(this).find('span').text('-');
+  }
 
-  });
-  $(".fade").click(function () {
-    $(".modal").fadeOut();
-    $(this).fadeOut();
-  })
-  $('a').click(function () {
-    var adjust = 0;
-    var speed = 400;
-    var href = $(this).attr("href");
-    var target = $(href == "#" || href == "" ? 'html' : href);
-    var position = target.offset().top + adjust;
-    $('body,html').animate({ scrollTop: position }, speed, 'swing');
-    return false;
-  });
+});
+$(".fade").click(function () {
+  $(".modal").fadeOut();
+  $(this).fadeOut();
+})
+$('a').click(function () {
+  var adjust = 0;
+  var speed = 400;
+  var href = $(this).attr("href");
+  var target = $(href == "#" || href == "" ? 'html' : href);
+  var position = target.offset().top + adjust;
+  $('body,html').animate({ scrollTop: position }, speed, 'swing');
+  return false;
+});
 
